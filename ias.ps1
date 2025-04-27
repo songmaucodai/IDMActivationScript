cd1b72cf040e6c46fcdf8fe01a19dbf689221624
Sống Màu Cỏ Dại <songmaucodai@gmail.com>
2025-04-27 18:58:07 +0700

    Create ias.ps1

---
+++ ias.ps1
@@ -0,0 +1,4 @@
+write-host "IDM-Activation-Script"
+write-host "https://github.com/songmaucodai/IDMActivationScript"
+write-host ""
+write-host "This project has been archived and is no longer being maintained."
